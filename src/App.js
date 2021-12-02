cbe0f764bfebba155b93dfb035e27e15db57934a
Matthew Edwards <matthew.edwards@Matthews-MacBook-Pro.local>
2021-12-02 14:56:35 -0600

    remove old keys

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import PNGeoMap from './components/PNGeoMap';
+import config from './config/keys.json';
 
 function App() {
   return (
@@ -9,9 +10,9 @@ function App() {
       </div>
       <div className="GeoMap">
         <PNGeoMap 
-          apiKey="AIzaSyAjLg0O7qLeuRrErhvn1qRH39UedsR8ua8"
-          publishKey='pub-c-14d052d1-9847-41a1-90ab-b75467751c0d'
-          subscribeKey='sub-c-d255c2c2-52c6-11ec-8b9e-da88ab2aee1c'
+          apiKey={config.apiKey}
+          publishKey={config.publishKey}
+          subscribeKey={config.subscribeKey}
           uuid='laptop-testing'
           initialCenter={
             {
